a63b763ce965c758f280b02f334101a45c17b12a
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-01-14 12:31:46 -0500

    Update HomeView.vue

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,1 +1,21 @@
-v
+<!-- get it through vbase-3-setup -->
+
+<template>
+    <h1>{{ count }}</h1>
+    <button @click="increment">Click Me</button>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+
+let count = ref(0)
+function increment() {
+    count.value++
+}
+
+</script>
+
+<style scoped>
+
+</style>
