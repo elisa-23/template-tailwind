a14ca75217d0ebd1781ba67e6199da37d59413b0
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-02-24 12:30:06 -0500

    ewreter

--- app/src/components/AnimalCards.vue
+++ app/src/components/AnimalCards.vue
@@ -1,16 +1,13 @@
 <template>
     <div>
-        <h2>{{  animal.name }}</h2>
-        <img :src="animal.image" alt="">
+
     </div>
 </template>
 
 <script setup>
-defineProps({
-    animal: Object,
-});
+
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
 </style>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,14 +1,25 @@
 <template>
     <div>
-        <AnimalCard v-for="animal in animals" :key="animal.name" :animal="animal" />
+        <div v-for="(mon, index) in pokemon" :key="mon.name">
+            <h2>{{ mon.name }}</h2>
+            <h3>{{ index + 1 }}</h3>
+        </div>
     </div>
 </template>
 
 <script setup>
-import AnimalCards from '@/components/AnimalCards.vue';
-const animals = [{name: "Walrus", image: "url"}, {name: "Mark Fridlin", image: "url1"}];
+import { ref, onMounted } from "vue";
+const pokemon = ref('');
+async function getPokemon() {
+    let res = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=151");
+    let data = await res.json();
+    pokemon.value = data.results;
+}
+onMounted(() => {
+    getPokemon();
+});
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
 </style>
